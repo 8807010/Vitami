3bfd708491beb31b06311e2f5fc6280b9e41bcb9
mat <lishay.matvey20001506matlish>
2021-08-05 17:38:27 +0300

    added some fixes in modal

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -2,36 +2,33 @@ export default function modal(btn) {
     let modalWindow = document.querySelector('.js-modal-window');
     let closeWindow = document.querySelector('.js-close');
 
+    const classes = {
+        openClass: 'is-open'
+    }
 
-    btn.addEventListener('click', (event) => {
-        modalWindow.style.cssText='display: flex';
+    btn.addEventListener('click', () => {
+        modalWindow.classList.add(classes.openClass)
     });
 
-    // btn.addEventListener('click', (event) => {
-    //     modalWindow.setAttribute('display: flex');
-    // });
-
-    closeWindow.addEventListener('click', (event) => {
-        modalWindow.style.cssText='display: none';
+    closeWindow.addEventListener('click', () => {
+        if (modalWindow.classList.contains(classes.openClass)) {
+            modalWindow.classList.remove(classes.openClass)
+        }
     });
 
-    window.addEventListener('resize', (event) => {
-        modalWindow.style.cssText='display: none'
+    window.addEventListener('resize', () => {
+        if (modalWindow.classList.contains(classes.openClass)) {
+            modalWindow.classList.remove(classes.openClass)
+        }
     })
 
-
-
     document.addEventListener('keypress', (event) => {
-        if(event.keyCode === 27) modalWindow.style.cssText='display: none';
-    }); 
-
-
+        if (event.keyCode === 27) modalWindow.style.cssText = 'display: none';
+    });
 
     document.addEventListener('keydown', (event) => {
         if (event.key === 'Escape') {
-            modalWindow.style.cssText='display: none';
+            modalWindow.style.cssText = 'display: none';
         }
-        });
-
-
+    });
 }
