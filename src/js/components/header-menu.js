6f1836678ae13fa0f3e430eaa8de341acd3bb441
Artiom <asokolik87@gmail.com>
2021-08-28 15:23:42 +0300

    menu mobile complete

--- src/js/components/header-menu.js
+++ src/js/components/header-menu.js
@@ -1,16 +1,11 @@
 export default function menuMobile(btn) {
+    
+    const classes = {
+        openClass: 'is-open'
+    }
 
     btn.addEventListener('click', (event) => {
-        btn.classList.toggle('is-open')
-
-        const classes = {
-            openClass: 'is-open'
-        }
-
-        if (btn.classList.contains(classes.openClass)) {
-            btn.classList.remove(classes.openClass)
-        } else {
-            btn.classList.add(classes.openClass)
-        }
+        let modal = document.querySelector('.js-header-modal');
+        modal.classList.toggle(classes.openClass);
     });
 }
